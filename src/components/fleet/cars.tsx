43cce6e97f7168060fed43b68a90e75b581dfca2
Francisco Victoriano <fvictoriano.dev@gmail.com>
2024-05-01 12:00:33 -0400

    added visual cue for length of the carousel

--- src/components/fleet/cars.tsx
+++ src/components/fleet/cars.tsx
@@ -3,17 +3,34 @@
 import { carFleet } from "../../lib/constants";
 import Card from "./card";
 import { cn } from "../../lib/utils";
-import { Dispatch, SetStateAction, useState } from "react";
-import { Carousel, CarouselContent, CarouselItem } from "../ui/carousel";
+import { Dispatch, SetStateAction, useState, useEffect } from "react";
+import {
+  Carousel,
+  CarouselApi,
+  CarouselContent,
+  CarouselItem,
+} from "../ui/carousel";
+
 export default function Cars() {
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
   const [active, setActive] = useState(0);
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+    setCurrent(api.selectedScrollSnap() + 1);
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
   return (
     <section className="flex flex-col items-center bg-white max-w-[1536px] pt-24 pb-24 px-10 mx-auto">
       <span className="B1 uppercase block mb-4">the cars</span>
       <h2 className="H1 mb-12 lg:mb-20 text-center lg:text-left">
         Our Impressive Fleet
       </h2>
-      <Carousel className="md:hidden w-[85%] mb-10 shadow-md rounded-xl">
+      <Carousel setApi={setApi} className="md:hidden w-[85%] mb-10 shadow-md rounded-xl">
         <CarouselContent>
           {carFleet.map((car) => (
             <CarouselItem key={`${car.name}_carousel`}>
@@ -28,6 +45,26 @@ export default function Cars() {
           ))}
         </CarouselContent>
       </Carousel>
+      <section className="md:hidden flex gap-x-2 items-center justify-between w-[85%] mb-6">
+        <div className={cn(`h-1 w-8 rounded bg-stone-200`,{
+          "bg-accent": current === 1
+        })}/>
+        <div className={cn(`h-1 w-8 rounded bg-stone-200`,{
+          "bg-accent": current === 2
+        })}/>
+        <div className={cn(`h-1 w-8 rounded bg-stone-200`,{
+          "bg-accent": current === 3
+        })}/>
+        <div className={cn(`h-1 w-8 rounded bg-stone-200`,{
+          "bg-accent": current === 4
+        })}/>
+        <div className={cn(`h-1 w-8 rounded bg-stone-200`,{
+          "bg-accent": current === 5
+        })}/>
+        <div className={cn(`h-1 w-8 rounded bg-stone-200`,{
+          "bg-accent": current === 6
+        })}/>
+      </section>
       <div className="hidden md:grid md:grid-cols-2 lg:grid-cols-3 gap-12 mb-10">
         {carFleet.map((car) => (
           <Card
